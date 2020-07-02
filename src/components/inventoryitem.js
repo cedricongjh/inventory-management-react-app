06cd176be8d20473d30eaa52a4a991936c2fcd6a
Cedric Ong <cedricong99@gmail.com>
2020-07-02 23:42:32 +0800

    modified inventory view component

--- src/components/inventoryitem.js
+++ src/components/inventoryitem.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function InventoryItem(props) {
     return (
-    <div className="ingredient">   
+    <div className="ingredient">
+    <img src={props.info.image} alt="ingredient"/>       
     <div>{props.info.name}</div>
     <div>{props.info.quantity} {props.info.measurement}</div>
     </div> 
--- src/components/items.js
+++ src/components/items.js
@@ -2,42 +2,51 @@ const items = [
                 {id : 1,
                 name: 'macadamias', 
                 quantity: 5, 
-                measurement: 'cup'},
+                measurement: 'cup',
+                image: "/images/macadamias.png"
+                },
 
                 {id : 2,
                 name: 'carrot',
                 quantity: 5,
-                measurement: 'pcs'},
+                measurement: 'pcs',
+                image: "/images/carrot.png"},
 
                 {id : 3,
                 name: 'nutritional yeast',
                 quantity: 5,
-                measurement: 'cup'},
+                measurement: 'cup',
+                image: "/images/nutritional_yeast.png"},
 
                 {id: 4,
                  name: 'white vinegar',
                  quantity: 100,
-                 measurement: 'tsp'},
+                 measurement: 'tsp',
+                 image: "/images/white_vinegar.png"},
 
                 {id: 5,
                  name: 'salt',
                  quantity: 100,
-                 measurement: 'tsp'},
+                 measurement: 'tsp',
+                 image: "/images/salt.png"},
 
                 {id : 6,
                  name: 'ground nutmeg',
                  quantity: 10,
-                 measurement: 'tsp'},
+                 measurement: 'tsp',
+                 image: "/images/ground_nutmeg.png"},
 
                 {id: 7,
                  name: 'pasta',
                  quantity: 10,
-                 measurement: 'servings'},
+                 measurement: 'servings',
+                 image: "/images/pasta.png"},
                  
                 {id: 8,
                  name: 'rolled oats',
                  quantity: 10,
-                 measurement: 'cup'}                
+                 measurement: 'cup',
+                 image: "/images/rolled_oats.png"}                
                 ] 
  
 export default items
